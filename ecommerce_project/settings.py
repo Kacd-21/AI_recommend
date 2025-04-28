c8ea45f8c6ea948080a10b040c852b6b08ed3d40
Cassidy <cazycoolminer@gmail.com>
2025-04-28 21:32:01 +0800

    Prepare project for Render production

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -25,6 +25,16 @@ SECRET_KEY = 'p_b^-p3!lz9rqokpcfh*zd$e@g$qsmml$5j4lm*%z+((r%%*ib'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Environment variables
+SECRET_KEY = os.getenv('m2@_7)5@2fz*mgm)vmd!sb_js+#y&$=$y(g@v5-=5k8vx_le6%', "fallback_dev_secret")
+DEBUG = False
+ALLOWED_HOSTS = ['ai-recommender-k6wy.onrender.com', 'localhost', '127.0.0.1']
+
+# Add your environment variables for MongoDB and OpenAI
+OPENAI_KEY = os.getenv("dab02bcf11b83362167ef1634b859e6a")
+MONGO_URI = os.getenv("d5f00a3894197fed3ab7df49986a603c")
+
+
 ALLOWED_HOSTS = []
 
 
